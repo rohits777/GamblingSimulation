2191ab268a9c2d19b05e68c40cbcda923134dca1
rohits777 <rohitashinde95@gmail.com>
2020-10-15 13:03:09 +0530

    Added: gambler winning and lossing percentage is added

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -3,19 +3,29 @@ import java.util.Random;
 public class GamblingSimulator {
 	 public static final int STAKE=100;
      public static final int BET=1;
-
+     public static final int WIN=50;
+     public static final int LOSE=51;
+    int i;
+        
+     int random=(int)Math.floor(Math.random()*10)%2;
 	public void winOrLose() 
-	  {
-		Random random= new Random();
-		int value= random.nextInt(2);
-      if (value==1) 
-      {
-    	  System.out.println("WIN");
-      }
-      else
-           System.out.println("Lose");
-        }
-	
+	 {
+		for (i=1;i<=100;i++) 
+		   { 
+	  
+	 switch(random)
+		{
+		case WIN:
+           System.out.println("***WIN***");
+		break;
+         
+		case LOSE:  
+           System.out.println("***Lose***");
+            break;
+		   }
+	   }
+       }
+	  
 	public static void main (String args[])
 	  {
 		 GamblingSimulator gamblingSimulator=new GamblingSimulator();
