49f780b189a2b941f7216ea4e4e50faf8f19394f
rohits777 <rohitashinde95@gmail.com>
2020-10-15 11:44:22 +0530

    Added: welcome messege is added

---
+++ GamblingSimulator.java
@@ -0,0 +1,9 @@
+package com.simulationgambling;
+
+public class GamblingSimulator {
+	 public static void main (String args[])
+	    {
+	      System.out.println("Welcome to Gambling Program");
+	      }
+
+}
